2ded4339250408bfbf7ed3678dab633e997b16e0
alirezashahab <shehabalireza@gmail.com>
2025-01-31 23:03:10 -0800

    update theme

--- lib/theme.dart
+++ lib/theme.dart
@@ -2,6 +2,42 @@ import 'package:flutter/material.dart';
 
 ThemeData lightThemeData() {
   return ThemeData(
+    inputDecorationTheme: InputDecorationTheme(
+      labelStyle: TextStyle(
+        fontSize: 14,
+      ),
+      contentPadding: EdgeInsets.all(
+        12,
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(4),
+        borderSide: BorderSide(
+          color: AppColor.mainTextColor,
+          width: 0.5,
+        ),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(4),
+        borderSide: BorderSide(
+          color: AppColor.mainColor,
+          width: 1,
+        ),
+      ),
+      errorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(4),
+        borderSide: BorderSide(
+          color: AppColor.redColor,
+          width: 0.5,
+        ),
+      ),
+      focusedErrorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(4),
+        borderSide: BorderSide(
+          color: AppColor.redColor,
+          width: 1,
+        ),
+      ),
+    ),
     scaffoldBackgroundColor: AppColor.scaffoldColor,
     textTheme: TextTheme(
         bodySmall: TextStyle(
